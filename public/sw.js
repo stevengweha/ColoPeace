c6c35c99d8b22b40a4ea9148acb50bccfa0ca928
stevengweha <ngwehasteve@gmail.com>
2026-01-22 23:36:37 +0100

    front-stable-V5

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 // public/sw.js - Service Worker ColoPeace v7
 // ==========================================
 
-const CACHE_NAME = 'colopeace-v9-stable'; 
+const CACHE_NAME = 'colopeace-v10-stable'; 
 
 // --- SYNC INTER-APP ---
 let lastNotificationTag = null;
