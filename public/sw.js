7c51d66d5748ce35b861906a4f18baca1aa6277a
stevengweha <ngwehasteve@gmail.com>
2026-01-23 23:57:45 +0100

    commit de test

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 // public/sw.js - Service Worker ColoPeace v7
 // ==========================================
 
-const CACHE_NAME = 'colopeace-v11-stable'; 
+const CACHE_NAME = 'colopeace-cache-satable-test'; 
 
 // --- SYNC INTER-APP ---
 let lastNotificationTag = null;
