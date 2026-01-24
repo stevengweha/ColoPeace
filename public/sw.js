41ca1c40c994df18471b378c967350a60ff198bb
stevengweha <ngwehasteve@gmail.com>
2026-01-24 01:38:53 +0100

    glephoto-v1

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 // public/sw.js - Service Worker ColoPeace v7
 // ==========================================
 
-const CACHE_NAME = 'colopeace-cache-satable-test-v7-122'; 
+const CACHE_NAME = 'sdsdsd-cache-satable-test-v7-dsdsd'; 
 
 // --- SYNC INTER-APP ---
 let lastNotificationTag = null;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -78,8 +78,7 @@ export default function Profile() {
       type: `image/${fileType === 'jpg' ? 'jpeg' : fileType}`, // Gestion du jpeg
     } as any);
 
-    const response = await api.post(`/users/upload-avatar/${user._id}`, formData, {
-    
+    const response = await api.post('/users/upload-avatar', formData, {
       // Très important pour axios avec FormData
       transformRequest: (data) => data, 
     });
